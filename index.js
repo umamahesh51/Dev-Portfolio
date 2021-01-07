db64eca43a2375bff9d1d3d482728b7ce28f01d7
Umamaheswararao Tumma <umamaheswararao.tumma@factset.com>
2021-01-07 13:42:48 +0530

    Added responsiveness to toggle bar for Mobile devices using View Pert meta tag and Setting the nav bar height dynamically uisng JQuery

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('.menu-toggle').click(function () {
         $('nav').toggleClass('active')
     })
+
+    $('nav').each(function () {
+        $(this).css({
+            'top':  $('header').height()
+        })
+    })
 })
+
